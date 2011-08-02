632859251583ee0cd96f7893935e614db692c889
Ben Foxall <benfoxall@gmail.com>
2011-08-02 01:15:28 +0100

    moved out js, ui tweaks and hooked up the solution finder - now 'kind of works' on iphones

--- assets/js/card_game.js
+++ assets/js/card_game.js
@@ -29,7 +29,7 @@ Array.prototype.$remove = function(inds){
 var operators = '+ - / *'.split(' ');
 
 // All the ways that the cards can be combined
-solutions = function(cards, cb){
+permutations = function(cards, cb){
   if(cards.length == 1){
     cb(cards[0]);
   }
@@ -39,95 +39,90 @@ solutions = function(cards, cb){
       var rest = cards.$remove([i,j]);
       for(var o = 0; o < operators.length; o++){
         var applied = ['(', cards[i], operators[o], cards[j], ')'].join(' ');
-        solutions(rest.$push(applied), cb);
+        permutations(rest.$push(applied), cb);
       }
 
     }
   }
 }
 
-var select_ids = ['card-1','card-2','card-3','card-4'];
 
-var check = function(){
-    var cards = [];
-    for (var i=0; i < select_ids.length; i++) {
-        cards[i] = parseInt(document.getElementById(select_ids[i]).innerHTML,0);
-    }
-
-  document.getElementById('results').innerHTML = '';
-  solutions(cards, function(solution){
-    //check if the solution reaches 21
-    if(eval(solution) === 21) {
-      document.getElementById('results').innerHTML += ('<li>' + solution + '</li>');
-    }
-  });
+var find_solutions = function(cards){
+	var sols = [];
+	permutations(cards, function(solution){
+		//check if the solution reaches 21
+		if(eval(solution) === 21) {
+			sols.push(solution);
+		}
+	});
+	return sols;
 }
 
 
 // -----------
 
-
 $(function(){
-  
-  $('#random').click(function(){
-      $('.card').each(function(){
-         var value = Math.min(Math.ceil(Math.random()*14), 10);
-         
-         // u - g - l -y
-         var cardname = value;
-         if(value == 1){
-             cardname = 'A';
-         }
-         if(value == 10){
-             cardname = ['J', 'Q', 'K'][Math.floor(Math.random()*3)];
-         }
-         
-         $(this).data('value', value).find('h2').text(cardname);
-      });
-  });
-  
-  $('#clear').click(function(){
-      $('.card').data('value',0).find('h2').text('');
-  });
-  
-  $('#choice li').click(function(e){
-      e.preventDefault();
-      var v = $(this).text();
-      $('.card').each(function(){
-          if(!$(this).data('value')){
-              $(this).data('value',v).find('h2').text(v);
-              return false;
-          }
-      });
-      
-  });
-  
-  $('#choice li, #clear, #random').click(function(){
-      var filled = !$('#cards li:empty').size();
-      $('#show-solution').attr('disabled',!filled).text('Find Solutions');
-      $('#results').hide();
+	
+    //a helper to set the content of a card
+    $.fn.card = function(v){
+        this.each(function(){
+            $(this).html(v + '<span class="flip">' + v + '</span>').data('value',v);
+        })
+    }
+    
+    $('.cards > li').each(function(){
+        $(this).card($(this).text());
+    });
 
-  });
-  
-  $('#show-solution').click(function(){
-      $('#results').hide();
-      if($(this).text() == 'Find Solutions'){
-          
-          check();
-          
-          var count = $('#results li').size();
-
-          $(this).text(count + ' Solution' + (count == 1 ? '' : 's') + ' (show)');
-          
-      } else {
-          $('#results').show();
-      }
-      
-  });
-  
-  $('#show-choice').click(function(){
-      $('#choice').toggle();
-  }).click();
-  
-  
-});
+	
+	$('.multiple .cards').each(function(){
+	    $('li', this).each(function(i){
+            $(this).css({position:'absolute',marginLeft:i + 'em',zIndex:100-i});
+	    });
+	});
+	
+    $('#options > li').click(function(){
+        var v = $(this).data('value');
+        if(v == 'random'){
+            $('#table li').each(function(){
+                $(this).card(Math.ceil(Math.random()*10))
+            });
+            
+            $('#options').removeClass('active');
+        } else {
+            var remaining = $('#table li:has(.flip:empty)');
+            if(remaining.size() == 1){
+                $('#options').removeClass('active');
+            }
+            remaining.first().card(v);
+        }
+    });
+    
+    $('nav a').click(function(e){
+        e.preventDefault();
+        var selector = $(this).attr('href');
+        
+        $(selector).toggleClass('active')
+                .siblings().removeClass('active');
+    });
+    $('nav a[href=#options]').click(function(){
+        $('#table li').card('');
+    });
+    $('nav a[href=#solutions]').click(function(){
+		
+		$('#solutions').empty();
+		
+		var cards = [];
+		$('#table li').each(function(){
+			cards.push(parseInt($(this).data('value'),10) || 0);
+		});
+		
+		var sols = find_solutions(cards);
+		$.each(sols, function(k,s){
+			$('#solutions').append('<li>' + s + '</li>');
+		});
+		if(sols.length == 0){
+			$('#solutions').html('<li>no solutions</li>');
+		}
+    })
+})
